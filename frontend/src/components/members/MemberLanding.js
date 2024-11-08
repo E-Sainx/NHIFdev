5105f2e83f8ba241b05e697057da428bf6ce2fa8
Blockchain <esaigilu@usiu.ac.ke>
2024-11-08 07:29:01 +0000

    Pictures and descriptions

--- frontend/src/components/members/MemberLanding.js
+++ frontend/src/components/members/MemberLanding.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { Slide, Fade } from 'react-awesome-reveal';
+import { Slide, Fade } from "react-awesome-reveal";
 
 const MemberLanding = () => {
   return (
     <>
       <Slide direction="down">
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
-          <h2 className="text-3xl font-bold text-customBlue mb-4">About SHA Management System</h2>
+          <img
+            src="/active.PNG"
+            alt="Active Icon"
+            class="w-auto h-auto lg:h-10 xl:h-16"
+          />
+
+          <h2 className="text-3xl font-bold text-customBlue mb-4">
+            About SHA Management System
+          </h2>
         </div>
       </Slide>
       <Slide direction="down">
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
           <Fade>
             <p className="text-gray-600">
-              The SHA Management System streamlines registration, contribution, and claim processes for members and healthcare providers, ensuring efficient and transparent healthcare fund management.
+              The Social Health Authority (SHA) is a government initiative
+              designed to manage health insurance in Kenya, replacing the
+              National Health Insurance Fund (NHIF). The primary objective of
+              SHA is to ensure that all Kenyans have access to affordable and
+              quality healthcare services.
             </p>
           </Fade>
         </div>
@@ -22,7 +34,12 @@ const MemberLanding = () => {
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
           <Fade>
             <p className="text-gray-600 mt-4">
-              Members can register, make contributions, and submit claims securely and transparently.
+              SHA Objective: Established under the Social Health Insurance Act
+              of 2023, SHA aims to provide accessible and affordable healthcare
+              for all Kenyans by managing three key funds:
+               <li> The Primary Healthcare Fund (PHF)</li>
+               <li> Social Health Insurance Fund (SHIF) </li>
+               <li> The Emergency, Chronic, and Critical Illness Fund.</li>
             </p>
           </Fade>
         </div>
@@ -30,7 +47,7 @@ const MemberLanding = () => {
       <Slide direction="down">
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
           <Fade>
-            <p className="text-gray-600 mt-4">Key features:</p>
+            <p className="text-gray-600 mt-4">Key features of the SHA system:</p>
             <ul className="list-disc list-inside text-gray-600 mt-2">
               <li>Secure contribution management</li>
               <li>Efficient claim processing</li>
@@ -43,10 +60,14 @@ const MemberLanding = () => {
       </Slide>
       <Slide direction="down">
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
-          <h2 className="text-3xl font-bold text-customBlue mb-4">Ethereum Blockchain Integration</h2>
+          <h2 className="text-3xl font-bold text-customBlue mb-4">
+            Ethereum Blockchain Integration
+          </h2>
           <Fade>
             <p className="text-gray-600">
-              Leveraging Ethereum blockchain ensures security, transparency, and immutability of transactions. Smart contracts automate processes, reducing errors and manual intervention.
+              Leveraging Ethereum blockchain ensures security, transparency, and
+              immutability of transactions. Smart contracts automate processes,
+              reducing errors and manual intervention.
             </p>
           </Fade>
         </div>
@@ -69,7 +90,9 @@ const MemberLanding = () => {
         <div className="bg-white shadow-md rounded-md p-6 mb-6 h-full">
           <Fade>
             <p className="text-gray-600 mt-4">
-              Ethereum integration ensures secure, transparent, and efficient handling of member contributions and provider claims, enhancing trust and accountability.
+              Ethereum integration ensures secure, transparent, and efficient
+              handling of member contributions and provider claims, enhancing
+              trust and accountability.
             </p>
           </Fade>
         </div>
